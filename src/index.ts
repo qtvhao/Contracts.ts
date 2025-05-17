0e063b08ca0d874aa9cb45b46d1af77938ca6096
Hao Nghiem <qtvhao@gmail.com>
2025-05-17 15:27:26 +0700

    Add

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { Application } from "./Domain/Contracts/Application";
+import { IDomainEvent } from "./Domain/Contracts/IDomainEvent";
 
-export { Application };
+export { Application, IDomainEvent };
