7c5263f61b79c7777f7a78e969ea5b63a9cf1f00
Hao Nghiem <qtvhao@gmail.com>
2025-05-19 12:55:23 +0700

    Add

--- src/Domain/Contracts/EventBus/IEventBus.ts
+++ src/Domain/Contracts/EventBus/IEventBus.ts
@@ -1,9 +1,7 @@
-import { IEventHandler } from "../../../Application/Handlers/Events/IEventHandler";
 import { IDomainEvent } from "../IDomainEvent";
 import { IEventPublisher } from "./IEventPublisher";
 import { IEventSubscriber } from "./IEventSubscriber";
 import { IInitializable } from "../Lifecycle/IInitializable";
-import { IEventMapperRegistry } from "./IEventMapperRegistry";
 
 export type EventConstructor<T extends IDomainEvent> = new (
   ...args: any[]
@@ -13,5 +11,4 @@ export interface IEventBus
   extends
     IEventPublisher,
     IEventSubscriber,
-    IEventMapperRegistry,
     IInitializable {}
