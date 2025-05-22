343ea0f0d841e1bc5a06f2465d5eaee5b69417e5
Hao Nghiem <qtvhao@gmail.com>
2025-05-23 01:12:50 +0700

    Add

--- src/Infrastructure/Bootstrap/types.ts
+++ src/Infrastructure/Bootstrap/types.ts
@@ -3,6 +3,7 @@ const TYPES = {
   MessageBrokerFactory: Symbol.for("MessageBrokerFactory"),
   EventMapperRegistry: Symbol.for("EventMapperRegistry"),
   EventBus: Symbol.for("EventBus"),
+  ConfigurationService: Symbol.for("ConfigurationService"),
 };
 
 export default TYPES;
