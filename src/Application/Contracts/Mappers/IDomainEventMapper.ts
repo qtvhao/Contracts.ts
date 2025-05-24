1db7c4eebee8a5e7c7c2592eccba0be7094625e1
Hao Nghiem <qtvhao@gmail.com>
2025-05-24 14:42:09 +0700

    Add

--- src/Application/Contracts/Mappers/IDomainEventMapper.ts
+++ src/Application/Contracts/Mappers/IDomainEventMapper.ts
@@ -3,4 +3,5 @@ import { IDomainEvent } from "../../../Domain/Contracts/IDomainEvent";
 export interface IDomainEventMapper<DTO, DomainEvent extends IDomainEvent> {
     toDomain(dto: DTO): DomainEvent;
     toDTO(event: DomainEvent): DTO;
+    isDomainEvent(event: IDomainEvent): event is DomainEvent;
 }
