b029a5e14486d24d96bd50aa8d766e898e7c0593
Hao Nghiem <qtvhao@gmail.com>
2025-05-24 18:46:32 +0700

    Add

--- src/Domain/Contracts/EventBus/IDomainEventMapperRegistry.ts
+++ src/Domain/Contracts/EventBus/IDomainEventMapperRegistry.ts
@@ -1,10 +1,11 @@
 import { IDomainEventMapper } from "../../../Application/Contracts/Mappers/IDomainEventMapper";
 import { IDomainEvent } from "../IDomainEvent";
+import { Message } from "./EachMessagePayload";
 
 /**
  * Registry for managing event mappers by event name.
  */
-export interface IDomainEventMapperRegistry<T extends IDomainEvent, U> {
+export interface IDomainEventMapperRegistry<T extends IDomainEvent, U extends Message> {
   get(eventName: string): IDomainEventMapper<U, T> | undefined;
   set(eventName: string, mapper: IDomainEventMapper<U, T>): void;
 }
