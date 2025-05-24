cab41af63f55d593fa9ea3a5ffa5bf17d27ff4ae
Hao Nghiem <qtvhao@gmail.com>
2025-05-24 11:04:56 +0700

    Add

--- src/Infrastructure/Bootstrap/types.0.ts
+++ src/Infrastructure/Bootstrap/types.0.ts
@@ -5,6 +5,7 @@ const TYPES = {
   EventBus: Symbol.for("EventBus"),
   ConfigurationService: Symbol.for("ConfigurationService"),
   EventTopicMapper: Symbol.for("EventTopicMapper"),
+  EventHandlerResolver: Symbol.for("EventHandlerResolver"),
 };
 
 export default TYPES;
