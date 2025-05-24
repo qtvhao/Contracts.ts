99b4fd3157d13ef59055114436b3f4d510d27f70
Hao Nghiem <qtvhao@gmail.com>
2025-05-24 14:56:38 +0700

    Add

--- src/Domain/Contracts/Messaging/IEventTopicMapper.ts
+++ src/Domain/Contracts/Messaging/IEventTopicMapper.ts
@@ -3,4 +3,5 @@ import { EventConstructor, IDomainEvent } from "../IDomainEvent";
 export interface IEventTopicMapper {
   getEventConstructor(topic: string): EventConstructor<IDomainEvent> | undefined;
   getTopicForEvent(event: EventConstructor<IDomainEvent>): string;
+  register(topic: string, constructor: EventConstructor<IDomainEvent>): void;
 }
