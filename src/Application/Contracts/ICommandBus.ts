49b14b6ab30cbee13fb34546dbcb9a8404d5a824
Hao Nghiem <qtvhao@gmail.com>
2025-05-25 21:39:45 +0700

    Add

--- src/Application/Contracts/ICommandBus.ts
+++ src/Application/Contracts/ICommandBus.ts
@@ -1,5 +1,7 @@
 import { ICommand } from "../Commands/ICommand";
+import { ICommandHandlerResolver } from "./ICommandHandlerResolver";
 
 export interface ICommandBus {
+  readonly handlerResolver: ICommandHandlerResolver;
   dispatch<T extends ICommand>(command: T): Promise<void>;
 }
