c14bba9f96f48f60c19558a8f02aceb2dbbf577f
Hao Nghiem <qtvhao@gmail.com>
2025-05-26 14:28:07 +0700

    Add

--- src/Application/Handlers/CQRS/Contracts/ICommandHandlerResolver.ts
+++ src/Application/Handlers/CQRS/Contracts/ICommandHandlerResolver.ts
@@ -1,8 +1,26 @@
-import { ICommand } from "../../../Commands/ICommand";
+import { CommandConstructor, ICommand } from "../../../Commands/ICommand";
 import { ICommandHandler } from "./ICommandHandler";
 
 export interface ICommandHandlerResolver {
-  resolve<TCommand extends ICommand, TResult extends object | void>(
-    command: TCommand
-  ): ICommandHandler<TCommand, TResult>;
+  /**
+   * Binds a command type to its handler.
+   * Enables dynamic handler lookup at runtime.
+   *
+   * @param query - Command constructor.
+   * @param handler - Handler instance for the command.
+   */
+  register<T extends ICommand>(
+    query: CommandConstructor<T>,
+    handler: ICommandHandler<T>,
+  ): void;
+
+  /**
+   * Retrieves the handler for a given command type.
+   *
+   * @param query - Command constructor.
+   * @returns Handler for the specified command.
+   */
+  resolve<T extends ICommand>(
+    query: CommandConstructor<T>,
+  ): ICommandHandler<T>;
 }
