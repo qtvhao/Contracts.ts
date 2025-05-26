95f88c746f824eb246534faa5b567bbfb3399769
Hao Nghiem <qtvhao@gmail.com>
2025-05-26 09:58:30 +0700

    Add

--- src/Infrastructure/Bootstrap/types.0.ts
+++ src/Infrastructure/Bootstrap/types.0.ts
@@ -2,14 +2,18 @@
 const TYPES = {
   CommandBus: Symbol.for("CommandBus"),
   QueryBus: Symbol.for("QueryBus"),
+  CommandHandlerResolver: Symbol.for("CommandHandlerResolver"),
+  QueryHandlerResolver: Symbol.for("QueryHandlerResolver"),
+  // 
   MessageBrokerFactory: Symbol.for("MessageBrokerFactory"),
   DomainEventMapperRegistry: Symbol.for("DomainEventMapperRegistry"),
   EventBus: Symbol.for("EventBus"),
-  ConfigurationService: Symbol.for("ConfigurationService"),
   EventTopicMapper: Symbol.for("EventTopicMapper"),
   EventHandlerResolver: Symbol.for("EventHandlerResolver"),
   EventBusFactory: Symbol.for("EventBusFactory"),
   MessageBrokerFactoryMap: Symbol.for("MessageBrokerFactoryMap"),
+  // 
+  ConfigurationService: Symbol.for("ConfigurationService"),
 };
 
 export default TYPES;
