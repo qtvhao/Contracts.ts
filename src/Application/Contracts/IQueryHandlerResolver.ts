3753f05a1b38c719d6fba1463e95eace6bcc7a32
Hao Nghiem <qtvhao@gmail.com>
2025-05-26 13:18:07 +0700

    Add

--- src/Application/Contracts/IQueryHandlerResolver.ts
+++ src/Application/Contracts/IQueryHandlerResolver.ts
@@ -2,23 +2,23 @@ import { IQuery, QueryConstructor } from "../Commands/IQuery";
 import { IQueryResult } from "../DTOs/IQueryResult";
 import { IQueryHandler } from "../Handlers/CQRS/Contracts/IQueryHandler";
 
-export interface IEventHandlerResolver {
+export interface IQueryHandlerResolver {
   /**
    * Registers a query handler for a specific query type.
-   * @param event - The constructor function of the query.
+   * @param query - The constructor function of the query.
    * @param handler - The query handler to associate with the query type.
    */
   register<T extends IQuery, TResult extends IQueryResult>(
-    event: QueryConstructor<T>,
-    handler: IQueryHandler<T, TResult>
+    query: QueryConstructor<T>,
+    handler: IQueryHandler<T, TResult>,
   ): void;
 
   /**
    * Resolves and retrieves the registered handler for a specific query type.
-   * @param event - The constructor function of the query.
+   * @param query - The constructor function of the query.
    * @returns The query handler associated with the query type.
    */
   resolve<T extends IQuery, TResult extends IQueryResult>(
-    event: QueryConstructor<T>
+    query: QueryConstructor<T>,
   ): IQueryHandler<T, TResult>;
 }
