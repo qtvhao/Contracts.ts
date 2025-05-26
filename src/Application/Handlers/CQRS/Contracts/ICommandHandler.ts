3044408212cd57af2abba82510742cc1762ebcd7
Hao Nghiem <qtvhao@gmail.com>
2025-05-26 14:41:08 +0700

    Add

--- src/Application/Handlers/CQRS/Contracts/ICommandHandler.ts
+++ src/Application/Handlers/CQRS/Contracts/ICommandHandler.ts
@@ -4,9 +4,5 @@ import { ICommand } from "../../../Commands/ICommand";
 export interface ICommandHandler<TCommand extends ICommand> {
   handle(command: TCommand): Promise<void>;
 
-  /**
-   * Optional hook to expose domain events generated during execution,
-   * aligning with Event Sourcing and EDA.
-   */
   getPublishedEvents(): ReadonlyArray<IDomainEvent>;
 }
