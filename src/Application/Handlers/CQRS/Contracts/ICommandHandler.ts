a5decb5b8c3de6825c50de4c8db41c07b4de1848
Hao Nghiem <qtvhao@gmail.com>
2025-05-27 20:07:04 +0700

    Add

--- src/Application/Handlers/CQRS/Contracts/ICommandHandler.ts
+++ src/Application/Handlers/CQRS/Contracts/ICommandHandler.ts
@@ -1,8 +1,5 @@
-import { IDomainEvent } from "../../../../Domain/Contracts/IDomainEvent";
 import { ICommand } from "../../../Commands/ICommand";
 
 export interface ICommandHandler<TCommand extends ICommand> {
   handle(command: TCommand): Promise<void>;
-
-  getPublishedEvents(): ReadonlyArray<IDomainEvent>;
 }
